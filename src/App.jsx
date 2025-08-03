c2e3058b9202a7d6e1fa71fb50ddc829bd4cbe37
Nicholas McCarroll <nickmccarroll8@gmail.com>
2025-08-03 17:20:27 -0400

    created temporary text screen, tailwind notes, component, and util folders

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class='flex flex-col items-center p-7 gap-10'>
+        <div class='text-5xl'>
+          Hobbing cell data underway...
+        </div>
+        <div class='text-green-600'>
+          Just gotta put the data into a js file
+        </div>
       </div>
-      <h1 class='text-green-500'>Vite + React + Nick</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
